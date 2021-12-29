c97401cf78842cc3caa40d74109a3ac8c6309502
szavinpeter1 <93372524+szavinpeter1@users.noreply.github.com>
2021-12-29 13:12:22 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
-basic.forever(function () {
-	
+let gun = {
+    x: 2,
+    y: 4
+}
+console.log(gun.y)
+
+led.plot(gun.x, gun.y)
+input.onButtonPressed(Button.A, function () {
+    if (gun.x > 0) {
+        led.unplot(gun.x, gun.y)
+        gun.x = gun.x - 1
+        led.plot(gun.x, gun.y)
+    } else {
+        console.log("x már 0")
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+    if (gun.x < 4) {
+        led.unplot(gun.x, gun.y)
+        gun.x = gun.x + 1
+        led.plot(gun.x, gun.y)
+    } else {
+        console.log("x már 4")
+    }
 })
