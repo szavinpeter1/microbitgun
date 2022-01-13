bb75468457b3a545f6ce5e660e6d8ca3a59e7695
szavinpeter1 <93372524+szavinpeter1@users.noreply.github.com>
2022-01-13 16:19:51 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@ let gun = {
     x: 2,
     y: 4
 }
-let bullet = {
-    x: gun.x,
-    y: gun.y
+
+function createBullet(x: number, y: number) {
+    return {
+        x: x,
+        y: y
+    }
 }
 
 led.plot(gun.x, gun.y)
@@ -12,10 +15,8 @@ input.onButtonPressed(Button.A, function() {
     if (gun.x > 0) {
         led.unplot(gun.x, gun.y)
         gun.x -= 1
-        bullet.x = gun.x
         led.plot(gun.x, gun.y)
         console.log(gun)
-        console.log(bullet)
     } else {
         console.log("x már 0")
     }
@@ -25,20 +26,19 @@ input.onButtonPressed(Button.B, function() {
     if (gun.x < 4) {
         led.unplot(gun.x, gun.y)
         gun.x += 1 
-        bullet.x = gun.x
         led.plot(gun.x, gun.y)
         console.log(gun)
-        console.log(bullet)
     } else {
         console.log("x már 4")
     }
 })
 
 input.onButtonPressed(Button.AB, function() {
+    let bullet = createBullet(gun.x, gun.y)
     while(bullet.y > -1){
         bullet.y--
         led.plot(bullet.x, bullet.y)
-        basic.pause(500)
+        basic.pause(80)
         led.unplot(bullet.x, bullet.y)
     }
     bullet.y = gun.y
